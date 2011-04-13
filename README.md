ff6cd78b28d036b31a23348aa0ab1590e0d65334
Aaron <aaron@9thport.net>
2011-04-12 21:32:24 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Bottles RailsApp
+======= ========
+
+Since I have become a home brewer and started to hand out beer in bottles to family and friends. I found out quickly that I needed an application to track my progress in brewing recipies and also a place for people who want beer, who have beer, who have returned bottles, and those who have not returned bottles.
+
+This application is setout to accomplish the above mentioned.
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bottles::Application.config.secret_token = 'b9ee9f433c0209fac7f7372be6579a49c11a8a8941403f048dc0fa73ef74419301a47a4e39532613ff0c2fc301c6fd4be659e9aa6f60415f1c8e91522e0e79d4'
